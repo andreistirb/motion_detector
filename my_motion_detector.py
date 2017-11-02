7f27c6a59263f04410a0f645f6a6597de976a7f8
Andrei Stirb <andrei.stirb@rinftech.com>
2017-11-02 14:41:59 +0200

    denoise the result of FFT and cleaned the code a bit

--- my_motion_detector.py
+++ my_motion_detector.py
@@ -8,7 +8,6 @@ Created on Mon Oct 23 13:23:43 2017
 import cv2
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
 import imutils
 
 #parse the arguments
@@ -17,10 +16,14 @@ ap.add_argument("-v", "--video", help="path to the video file")
 args = vars(ap.parse_args())
 
 camera = cv2.VideoCapture(args["video"])
-camera.set(0 , 100000)
+camera.set(0, 150000)
 
 img_array = []
 orig_array = []
+
+#take each frame of the video and build two arrays,
+#one with original frames and the other with blurred
+#gray frames
 
 while True:
     (grabbed, frame) = camera.read()
@@ -31,24 +34,15 @@ while True:
     frame = imutils.resize(frame, width=300)
     
     orig_array.append(frame)
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (11,11), 0)
-    
     img_array.append(blur)
     
-    #cv2.imshow("Original", frame)
-    #cv2.imshow("Blurred", blur)
-
-   # key = cv2.waitKey(25) & 0xFF
-                     
-    #if key == ord("q"):
-        #break
-    
-    
 X = np.array(img_array)
+
+#we need this only for displaying the result
 O = np.array(orig_array)
-print(X.size)
-print(X.shape)
 
 #3D fast fourier transform on the whole sequence of frames
 q = np.fft.fftn(X)
@@ -64,11 +58,11 @@ reconstructed_array = np.fft.ifftn(phase_spectrum_array)
 
 #reconstruct the frames of the video
 q= 0
-for i in range(0,X.shape[0]):
+for i in range(0,O.shape[0]):
     #smooth the frame using the averaging filter
     frame = abs(reconstructed_array[i])
     
-    filteredFrame = cv2.GaussianBlur(frame, (15,15), 0)
+    filteredFrame = cv2.GaussianBlur(frame, (5,5), 0)
     
     #convert the frame into binary image using mean value as threshold
     mean_value = np.mean(filteredFrame)
@@ -76,34 +70,44 @@ for i in range(0,X.shape[0]):
     #median_value = np.median(filteredFrame)
     ret, binary_frame = cv2.threshold(filteredFrame, 1.8*mean_value, 255, cv2.THRESH_BINARY) #previous alpha of mean = 1.6
     
-    #perform morphological operations
+    #denoise the binary_frame
+    npbinary = np.array(binary_frame, dtype = np.uint8)
+    denoised = cv2.fastNlMeansDenoising(src=npbinary, h=200, templateWindowSize=7, searchWindowSize=21)
     
-    kernel = np.ones((5,5), np.uint8)
+    #perform morphological operations
+    kernel = np.ones((13,13), np.uint8)
     
     closing = cv2.morphologyEx(binary_frame, cv2.MORPH_CLOSE, kernel)
     opening = cv2.morphologyEx(binary_frame, cv2.MORPH_OPEN, kernel)
+                
+    (_, cnts, _) = cv2.findContours(denoised, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+	 #loop over the contours
+    for c in cnts:
+		 #if the contour is too small, ignore it
+        if cv2.contourArea(c) < 100:
+            continue
+
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(O[i], (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(denoised, (x, y), (x + w, y + h), (255, 255, 0), 2)
     
-    
-    if q==0:
-        print(closing)
-        q = 1
-    #cv2.imshow("abs", frame)
-    
+    cv2.imshow("Denoised", denoised)
     cv2.imshow("Binary", binary_frame)
     cv2.imshow("Opening", opening)
-    cv2.imshow("Blurred", X[i])
     cv2.imshow("Original", O[i])
     cv2.imshow("Closing", closing)
     
-    key = cv2.waitKey(10) & 0xFF
+    key = cv2.waitKey(30) & 0xFF
                      
     if key == ord("q"):
         break
 
+    #superimpose segmented masks on its respective frames to obtain moving objects - search the link
     
-    #superimpose segmented masks on its respective frames to obtain moving objects
-    
-    
+cv2.destroyAllWindows()
 
 
                      
